2df3273fc1b20c2dc4abeee0bce44dfc108be5cb
shreedhavastra19 <shreedhavastra19@gmail.com>
2026-07-20 07:24:26 +0530

    update paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -2,14 +2,14 @@
 // Shreedha Vastra — Payment Routes
 // ================================================================
 import express from 'express';
-import { protect } from '../middleware/authMiddleware.js';
+import { protectOptional } from '../middleware/authMiddleware.js';
 import { createRazorpayOrder, verifyRazorpayPayment } from '../controllers/paymentController.js';
 
 const router = express.Router();
 
 router.use(protect);
 
-router.post('/create-order', createRazorpayOrder);
-router.post('/verify', verifyRazorpayPayment);
+router.post('/create-order',proctectOptional, createRazorpayOrder);
+router.post('/verify', protectOptional , verifyRazorpayPayment);
 
 export default router;
