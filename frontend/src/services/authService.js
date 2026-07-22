335cad990735eb7d65758cf4f6b3fdaf978d03c0
shreedhavastra19 <shreedhavastra19@gmail.com>
2026-07-22 17:22:38 +0530

    fix don't show error toast for silent guest session check

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -4,7 +4,7 @@ const authService = {
   register: (data) => api.post('/auth/register', data).then((res) => res.data),
   login: (data) => api.post('/auth/login', data).then((res) => res.data),
   logout: () => api.post('/auth/logout').then((res) => res.data),
-  getMe: () => api.get('/auth/me').then((res) => res.data),
+  getMe: () => api.get('/auth/me',{silent:true}).then((res) => res.data),
   forgotPassword: (email) => api.post('/auth/forgot-password', { email }).then((res) => res.data),
   resetPassword: (token, password) =>
     api.put(`/auth/reset-password/${token}`, { password }).then((res) => res.data),
